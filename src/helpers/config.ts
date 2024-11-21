0463a5298ad8945b3527be91578ee38cb3ede1ca
Eliaby Teixeira <eliaby.teixeira@gmail.com>
2024-11-21 19:28:57 -0300

    remove LoginContext (#1)

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,12 +1,12 @@
 export function setEnviroment() {
-	const environments = {
-		prod: "https://koral-api.koin.com.br",
-		"dev-1": "https://dev-1-koral-api.koin.com.br",
-		"pre-prd": "https://pre-prd-koral-api.koin.com.br",
-		local: "https://dummyjson.com/auth",
-	};
+  const environments = {
+    'prod': 'https://koral-api.koin.com.br',
+    'dev-1': 'https://dev-1-koral-api.koin.com.br',
+    'pre-prd': 'https://pre-prd-koral-api.koin.com.br',
+    'local': 'https://dummyjson.com/auth',
+  }
 
-	const env = process.env.REACT_APP_ENVIRONMENT;
+  const env = process.env.REACT_APP_ENVIRONMENT
 
-	return environments[env];
+  return environments[env]
 }
